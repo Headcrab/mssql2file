307e17d92644137900c44b0edbadf70c7f3c28fa
Александр Новиков <xtrim.work@gmail.com>
2023-01-25 19:41:15 +0600

    mssql2clickhouse

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/ClickHouse/clickhouse-go/v2"
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func main() {
-	// Set end time to current 20.01.2023 18:00:00
-	end, _ := time.Parse("2006-01-02 15:04:05", "2023-01-23 12:00:00")
+	// Set end time to current 2023-01-23 12:00:00
+	end, _ := time.Parse("2006-01-02 15:04:05", "2023-01-23 20:00:00")
 
 	// Connect to MSSQL source database
 	sourceDb, err := sql.Open("mssql", "server=139.158.31.1;port=1433;user id=sa;password=!QAZ1qaz12345;database=runtime;TrustServerCertificate=true;encrypt=disable;connection timeout=3000;")
@@ -20,34 +22,28 @@ func main() {
 	}
 	defer sourceDb.Close()
 
-	// Connect to MSSQL destination database
-	destDb, err := sql.Open("mssql", "server=localhost;port=1433;user id=sa;password=!QAZ1qaz12345;database=runtime;TrustServerCertifier=true;encrypt=disable;connection timeout=3000;")
+	// Connect to ClickHouse destination database
+	// destDb, err := clickhouse.OpenDirect("tcp://127.0.0.1:9000?debug=true")
+	destDb, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{"localhost:9000"},
+		Auth: clickhouse.Auth{
+			Database: "default",
+			Username: "default",
+			Password: "password123",
+		},
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer destDb.Close()
 
-	// Prepare destination insert statement
-	destInsert, err := destDb.Prepare("INSERT INTO runtime.dbo.history (tag_id, [date], [value]) VALUES (?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer destInsert.Close()
-
-	// Prepare destination tag insert statement
-	destInsertTag, err := destDb.Prepare("INSERT INTO tagname (tag) VALUES (?) SELECT SCOPE_IDENTITY()")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer destInsertTag.Close()
-
-	// Create a map to cache the tag names and their corresponding IDs
-	tagCache := make(map[string]int)
+	// Set context
+	ctx := context.Background()
 
 	period := -time.Minute * 5
 	start := end.Add(period)
 	for i := 0; i < 8760; i++ {
-		// Copy data from MSSQL source to MSSQL destination for each hour
+		// Copy data from MSSQL source to ClickHouse destination for each hour
 		startStr := start.Format("2006-01-02 15:04:05")
 		endStr := end.Format("2006-01-02 15:04:05")
 		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), " : ", startStr, " - ", endStr)
@@ -67,26 +63,15 @@ func main() {
 				log.Println(err.Error())
 			}
 
-			// Check if tag is already in cache
-			var tagID int
-			var ok bool
-			if tagID, ok = tagCache[tag]; !ok {
-				// Tag not in cache, check the destination database
-				destInsertTag.QueryRow(tag).Scan(&tagID)
-				// if err != nil {
-				// 	log.Println(err.Error())
-				// }
-				// Add the new tag to the cache
-				tagCache[tag] = tagID
+			// Insert data into destination ClickHouse
+			destDb.Exec(ctx, "INSERT INTO history (tag_name, date, value) VALUES (?, ?, ?)", tag, date, value)
+			//  Exec("INSERT INTO history (tag_name, date, value) VALUES (?, ?, ?)")
+			//  Query("INSERT INTO history (tag_name, date, value) VALUES (?, ?, ?)", tag, date, value)
+			if err != nil {
+				log.Println(err.Error())
 			}
-
-			// Insert data into destination with the tag ID
-			destInsert.Exec(tagID, date, value)
-			// if err != nil {
-			// 	log.Println(err.Error())
-			// }
 		}
-		start = start.Add(period)
-		end = end.Add(period)
+		start = start.Add(time.Hour)
+		end = end.Add(time.Hour)
 	}
 }
